3ee624157b5deaa7a65e8d33f16327026dadbf97
lshu <>
2023-12-21 18:02:22 +0800

    update

--- cloud/src/components/layout/SideBar.vue
+++ cloud/src/components/layout/SideBar.vue
@@ -1,13 +1,32 @@
 <script setup>
+import { reactive } from 'vue';
 import { useSettingStore } from '../../stores/setting';
 
 const settingStore = useSettingStore();
+
+const items = reactive([
+    { text: 'My Files', icon: 'mdi-folder' },
+    { text: 'Shared with me', icon: 'mdi-account-multiple' },
+    { text: 'Starred', icon: 'mdi-star' },
+    { text: 'Recent', icon: 'mdi-history' },
+    { text: 'Offline', icon: 'mdi-check-circle' },
+    { text: 'Uploads', icon: 'mdi-upload' },
+    { text: 'Backups', icon: 'mdi-cloud-upload' },
+]);
 </script>
 
 <template>
-    <v-navigation-drawer v-model="settingStore.settings.isCollapse" color="grey-darken-2" permanent
-        >nemus</v-navigation-drawer
-    >
+    <v-navigation-drawer v-model="settingStore.settings.isCollapse" color="grey-darken-2" permanent>
+        <v-list :lines="false" density="compact" nav>
+            <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary">
+                <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                </template>
+
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 <style scoped lang="scss"></style>
